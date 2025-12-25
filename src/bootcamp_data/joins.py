c7a30eb224a4b15d912306fad8a6d89bfeb1fa0e
teifhrbi1 <123744141+teifhrbi1@users.noreply.github.com>
2025-12-25 19:53:42 +0300

    w2d4: update eda notebook

--- src/bootcamp_data/joins.py
+++ src/bootcamp_data/joins.py
@@ -1,13 +1,10 @@
 import pandas as pd
 
 def safe_left_join(left, right, on, validate, suffixes=("", "_right")):
-    """
-    دمج الجداول بأسلوب آمن للتأكد من عدم تكرار البيانات
-    """
     return left.merge(
         right, 
         on=on, 
-        how="left", 
+      how="left", 
         validate=validate, 
         suffixes=suffixes
     )
